76502be2a13de6915f51b7bc49b9e4759a7b654c
arthurpex <arthurpex@gmail.com>
2022-03-22 21:29:09 -0300

    Inital commit

---
+++ app.py
@@ -0,0 +1,52 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+from flask import Flask, render_template
+load_dotenv()
+
+# URL2 = "https://portal.sensebike.com.br/glstorefront/glsense/pt/BRL/Cor/BICICLETA-CRITERIUM-COMP-2021-22---SENSE/p/160639_base/orderForm?warehouseCode=1101"
+SECRET_KEY = os.getenv('SECRET_KEY')
+VERSA_COMP = os.getenv('VERSA_COMP')
+VERSA_EVO = os.getenv('VERSA_EVO')
+
+def get_stock_values(bike) -> dict:
+    res = requests.get(bike)
+    soup = BeautifulSoup(res.content, 'html.parser')
+
+    stock_rows = soup.findAll('td', class_='hidden-xs')
+    stock_obj = {
+        0 : {'size':'49', 'qtd': None },
+        1: {'size':'52', 'qtd': None},
+        2: {'size':'55', 'qtd': None},
+        3: {'size':'58', 'qtd': None}
+    }
+
+    for index, item in enumerate(stock_rows):
+        in_stock = item.find('span', class_='td_stock in-stock')
+
+        if in_stock:
+            amount = int(in_stock.find('span').contents[0])
+            stock_obj[index]['qtd'] = amount
+    return stock_obj.values()
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY=SECRET_KEY,
+    )
+
+    @app.route('/')
+    def comp() -> dict:
+        data = get_stock_values(VERSA_COMP)
+        # data = {'size': '58', 'qtd': 10}
+        return render_template('comp.html', data=data, url=VERSA_COMP)
+
+    @app.route('/evo')
+    def evo() -> dict:
+        data = get_stock_values(VERSA_EVO)
+        # data = {'size': '58', 'qtd': 10}
+        return render_template('evo.html', data=data, url=VERSA_EVO)
+
+    return app
